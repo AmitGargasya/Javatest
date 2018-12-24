0c4758e6f113a2b728cd5954b69507cf90d522d2
Amit Kumar Suar <amit.suar@SLS.ads.valuelabs.net>
2018-12-24 15:20:44 +0530

    changes 24

--- travis_start.sh
+++ travis_start.sh
@@ -1,4 +1,4 @@
-  function docker_image_creation(args: string tag_name, string classname){
-  - docker build -f Dockerfile -t ${tag_name} --build-arg arg="${classname}" .
-  - docker run ${tag_name}
-  }
+tag_name = $1
+classname = $2
+- docker build -f Dockerfile -t ${tag_name} --build-arg arg="${classname}" .
+- docker run ${tag_name}
